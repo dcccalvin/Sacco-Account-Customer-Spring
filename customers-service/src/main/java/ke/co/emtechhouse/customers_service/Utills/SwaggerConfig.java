8334a7832316d67f72c9001782893323c8ceac70
Calvin <chumacalvin8@gmail.com>
2025-06-30 09:30:31 +0300

    Custom Swagger Config for CustomerService

---
+++ customers-service/src/main/java/ke/co/emtechhouse/customers_service/Utills/SwaggerConfig.java
@@ -0,0 +1,54 @@
+package ke.co.emtechhouse.customers_service.Utills;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Custom Sacco Swagger API")
+                        .description("Customer Service.")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Calvin")
+                                .email("chumacalvin8@gmail.com")
+                                .url("")
+
+                        ).license(new License()
+                                .name("MIT")
+                                .url("")
+                        )
+                );
+
+
+    }
+//    @Bean
+//    public OperationCustomizer addGlobalHeaders() {
+//        return (operation, handlerMethod) -> {
+//            operation.addParametersItem(new Parameter()
+//                    .in(ParameterIn.HEADER.toString())
+//                    .name("userName")
+//                    .description("Remote User")
+//                    .required(true)
+//                    .schema(new Schema<String>().type("string")));
+//            operation.addParametersItem(new Parameter()
+//                    .in(ParameterIn.HEADER.toString())
+//                    .name("entityId")
+//                    .description("Entity ID")
+//                    .required(true)
+//                    .schema(new Schema<String>().type("string")));
+//            return operation;
+//        };
+//    }
+}
